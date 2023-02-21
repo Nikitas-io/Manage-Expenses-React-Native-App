c8e70c443216e93a670663dae42ad416f46bfa93
Nikitas I/O <chrononautic@gmail.com>
2023-02-21 11:36:02 +0200

    Fetching stored items from DB and storing them to context store

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,13 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
+
+
 
 function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext);
+  const expensesContext = useContext(ExpensesContext);
 
-  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+  useEffect(() => {
+    // The useEffect function itself should not be asynchronous because it would 
+    // return a promise and useEffect must not return a promise. Instead, we create
+    // another async function to call the fetchExpenses async function.
+    async function getExpenses() {
+      // Get the expenses asynchronously.
+      const expenses = await fetchExpenses();
+      // Save the expenses to the context store.
+      expensesContext.setExpenses(expenses);
+    }
+
+    // Call the async function.
+    getExpenses();
+
+  }, []); 
+
+  const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
 
--- util/http.js
+++ util/http.js
@@ -1,8 +1,34 @@
 import axios from 'axios';
 
+const BACKEND_ROOT_URL = 'https://expense-list-testing-default-rtdb.europe-west1.firebasedatabase.app';
+
 export function storeExpense(expenseData) {
     axios.post(
-        'https://expense-list-testing-default-rtdb.europe-west1.firebasedatabase.app/expenses.json',
+        BACKEND_ROOT_URL + "/expenses.json",
         expenseData
-    )
+    );
+}
+
+export async function fetchExpenses() {
+    // Since this function is asynchronous, we await for the response.
+    const response = await axios.get(BACKEND_ROOT_URL + "/expenses.json");
+
+    // Initialize an array to hold the expenses fetched from the backend.
+    const expenses = [];
+
+    console.log('The response data: ', response.data)
+
+    // Loop through the response object's data.
+    for (const key in response.data) {
+        const expenseObj = {
+            id: key,
+            amount: response.data[key].amount,
+            date: new Date(response.data[key].date),
+            description: response.data[key].description
+        }
+
+        expenses.push(expenseObj)
+    }
+
+    return expenses;
 }
